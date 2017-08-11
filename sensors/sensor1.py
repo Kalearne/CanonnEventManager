8fefca985dc0edccd0b02f4bf53be257bb770f39
Kalearne <30927503+Kalearne@users.noreply.github.com>
2017-08-12 00:34:57 +0100

    removal of upper case
    Failed to register pack "CanonnEventManager": Pack ref / name can only contain valid word characters (a-z, 0-9 and _), dashes are not allowed.

--- sensors/sensor1.py
+++ sensors/sensor1.py
@@ -15,10 +15,10 @@ class HelloSensor(Sensor):
     def run(self):
         while not self._stop:
             self._logger.debug('GreetingSensor dispatching trigger...')
-            count = self.sensor_service.get_value('CanonnEventManager.count') or 0
+            count = self.sensor_service.get_value('canonneventmanager.count') or 0
             payload = {'greeting': 'Helloooo!', 'count': int(count) + 1}
-            self.sensor_service.dispatch(trigger='CanonnEventManager.event1', payload=payload)
-            self.sensor_service.set_value('CanonnEventManager.count', payload['count'])
+            self.sensor_service.dispatch(trigger='canonneventmanager.event1', payload=payload)
+            self.sensor_service.set_value('canonneventmanager.count', payload['count'])
             eventlet.sleep(60)
 
     def cleanup(self):
